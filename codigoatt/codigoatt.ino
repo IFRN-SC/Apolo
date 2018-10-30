aca5fed737eb056e8db5a464b10c3b80e93bb6fb
GilberthGabriel <dev.gilberth@gmail.com>
2018-10-30 10:22:02 -0300

    Adicionada condição para fazer curvas no verde

--- codigoatt/codigoatt.ino
+++ codigoatt/codigoatt.ino
@@ -7,8 +7,10 @@ float delayTwoEsq = 440; // Tempo que o robô passa girando
 float delayOneDir = 490;
 float delayTwoDir = 465;
 
-int pinMaisEsq = 3;
-int pinEsq = 2;
+float verde[2] = {0, 0};
+
+int pinMaisEsq = 2;
+int pinEsq = 3;
 int pinDir = 1;
 int pinMaisDir = 0;
 
@@ -25,24 +27,7 @@ void loop() {
     btnSeguirLinha = robo.botao3Pressionado();
 
     if (btnSubirRampa == true) {
-        robo.lerCalibracao(val);
-        valorCorte[0] = val.refletanciaMaisEsq;
-        valorCorte[1] = val.refletanciaEsq;
-        valorCorte[2] = val.refletanciaDir;
-        valorCorte[3] = val.refletanciaMaisDir;    
-        for (int i = 0; i < 4; i++) {
-            Serial.print("VALOR DE CORTE ");
-            Serial.print(i);
-            Serial.print(" : ");
-            Serial.println(valorCorte[i]);
-        }
-        if (rampa) {
-            rampa = false;      
-            robo.desligarLed(1);
-        } else {
-            rampa = true;
-            robo.ligarLed(1);
-        }    
+        calibrarVerde();        
     } else if (btnCalibrarBranco == true) {    
         calibrarBranco();
         delay(5000);
@@ -77,7 +62,7 @@ void loop() {
     }
     
     if (seguirLinha) {            
-        if ((robo.lerSensorSonarEsq() < 10) && (robo.lerSensorSonarDir() < 10)) {
+        if ((robo.lerSensorSonarEsq() < 10)) {
             initRampa();
         } else if (robo.lerSensorSonarFrontal() < 10) {
            desviarObstaculo();
@@ -177,10 +162,7 @@ void calibrarBranco() {
     Serial.println("Branco calibrado");
 }
 
-void initSeguirLinha() {      
-    int delay90 = 0;
-    int delayCurva = 0;
-  
+void initSeguirLinha() {        
     float sensores[4] = {0, 0, 0, 0};
     sensores[0] = (analogRead(pinMaisEsq)/1023.0)*100.0;
     sensores[1] = (analogRead(pinEsq)/1023.0)*100.0;
@@ -197,29 +179,32 @@ void initSeguirLinha() {
         delay(delayOneDir);
         robo.acionarMotores(-30,30);    
         delay(delayTwoDir);
+    } else if (sensores[1] > (verde[1]-4) && sensores[1] < (verde[1]+4)) { // VERDE ESQ
+        delay(delayOneEsq);
+        robo.acionarMotores(30,-30);
+        delay(delayTwoEsq);
+    } else if (sensores[2] > (verde[2]-4) && sensores[2] < (verde[2]+4)) { // VERDE DIR
+      delay(delayOneDir);
+        robo.acionarMotores(-30,30);    
+        delay(delayTwoDir);
     } else if (sensores[2] < valorCorte[2] && sensores[1] > valorCorte[1]){ // DIR = branco e ESQ = preto        
         // ====CURVA À ESQUERDA===="
-        robo.acionarMotores(-35,35); // Curva à esquerda        
+        robo.acionarMotores(-10,10); // Curva à esquerda        
     } else if (sensores[2] > valorCorte[2] && sensores[1] < valorCorte[1]){ // DIR = preto e ESQ = branco            
         // ====CURVA À DIREITA====
-        robo.acionarMotores(35,-35); // Curva à direita
+        robo.acionarMotores(10,-10); // Curva à direita
     } else if (sensores[3] > valorCorte[3] && sensores[0] < valorCorte[0]) { // +DIR = preto ou +ESQ = branco        
         // ====CURVA À ESQUERDA====
         robo.acionarMotores(35,-35);        
     } else if (sensores[3] < valorCorte[3] && sensores[0] > valorCorte[0]) { // +DIR = branco e +ESQ = preto                
         Serial.println("====CURVA À DIREITA====");
         robo.acionarMotores(-35,35);                
-    } else if(sensores[0] > valorCorte[0] && sensores[3] > valorCorte[3]) {        
-        robo.acionarMotores(33,30);
-    } else if (sensores[1] < valorCorte[1] && sensores[2] < valorCorte[2]) {
+    } else {
         robo.acionarMotores(33,30);
     }
 }
 
 void initRampa() {        
-    int delay90 = 0;
-    int delayCurva = 0;
-  
     float sensores[4] = {0, 0, 0, 0};
     sensores[0] = (analogRead(0)/1023.0)*100.0;
     sensores[1] = (analogRead(1)/1023.0)*100.0;
@@ -227,47 +212,72 @@ void initRampa() {
     sensores[3] = (analogRead(3)/1023.0)*100.0;
     
     if (sensores[2] < valorCorte[2] && sensores[1] > valorCorte[1]){ // DIR = branco e ESQ = preto
-        robo.acionarMotores(30,0); // Curva à esquerda
-        delay(delayCurva);
+        robo.acionarMotores(40,-5); // Curva à esquerda
     } else if (sensores[2] > valorCorte[2] && sensores[1] < valorCorte[1]){ // DIR = preto e ESQ = branco    
-        robo.acionarMotores(0,30); // Curva à direita
-        delay(delayCurva);
+        robo.acionarMotores(-5,40); // Curva à direita
     } else if (sensores[3] > valorCorte[3] && sensores[0] < valorCorte[0]) { // +DIR = preto ou +ESQ = branco
-        robo.acionarMotores(0,40);
-        delay(delay90);      
+        robo.acionarMotores(-5,40);
     } else if (sensores[3] < valorCorte[3] && sensores[0] > valorCorte[0]) { // +DIR = branco e +ESQ = preto
-        robo.acionarMotores(40,0);
-        delay(delay90);    
+        robo.acionarMotores(40,-5);
     } else {
-        robo.acionarMotores(53,50);
+        robo.acionarMotores(60,60);
     }
 }
 
+void calibrarVerde() {
+    boolean led = false;
+     for (int j=0; j<10; j++) {
+        verde[0] += (analogRead(1)/1023.0)*100.0;
+        delay(100);
+        if (led) {
+          led = false;
+          robo.desligarLed(2);
+        } else {
+          led = true;
+          robo.ligarLed(2);
+        }
+     }    
+    verde[0] /= 10;
+    delay(5000);
+    for (int j=0; j<10; j++) {
+        verde[1] += (analogRead(2)/1023.0)*100.0;
+        delay(100);
+        if (led) {
+          led = false;
+          robo.desligarLed(2);
+        } else {
+          led = true;
+          robo.ligarLed(2);
+        }
+     }    
+    verde[1] /= 10;
+}
+
 void desviarObstaculo() {
-    robo.acionarMotores(-10,-10);
+  robo.acionarMotores(-10,-10);
     delay(1000); 
-    robo.acionarMotores(-35,40); // Gira para a direita
+    robo.acionarMotores(-40,40); // Gira para a direita
     delay(350);
     robo.acionarMotores(0,0);
     delay(1000);
     robo.acionarMotores(40,35); // Vai para frente
-    delay(600); 
+    delay(550); 
     robo.acionarMotores(0,0);
     delay(1000);
-    robo.acionarMotores(40,-45); // Gira a esquerda
+    robo.acionarMotores(40,-40); // Gira a esquerda
     delay(300);    
     robo.acionarMotores(0,0);
     delay(1000);
     robo.acionarMotores(40,35); // Vai pra frente
-    delay(900);
+    delay(880);
+    //robo.acionarMotores(0,0);
+    //delay(1000);
+    robo.acionarMotores(40,-20); // Gira para esquerda
+    delay(500);
+    robo.acionarMotores(40,50); 
+    delay(85);
+    robo.acionarMotores(-10,30);
+    delay(350);
     robo.acionarMotores(0,0);
-    delay(1000);
-    robo.acionarMotores(40,-35); // Gira para esquerda
-    delay(400);
-    robo.acionarMotores(40,40); 
-   delay(400);
-    robo.acionarMotores(-10,10);
-    delay(480);
-    robo.acionarMotores(0,0); 
-    delay(3000);  
+    delay(3000); 
 }
